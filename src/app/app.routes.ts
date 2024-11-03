bdc03eecfa7525a4c3b57718aa43ca5fd4cad846
Muditha256 <muditharandeep123@gmail.com>
2024-11-03 13:10:37 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppoinmentManageComponent } from './appoinment-manage/appoinment-manage.component';
+import { PatientManageComponent } from './page/patient-manage/patient-manage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"appoinment",
+        component:AppoinmentManageComponent
+    },
+    {
+        path:"patient",
+        component:PatientManageComponent
+    }
+];
