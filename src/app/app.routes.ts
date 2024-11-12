14579e5d95978ddbe5bcdce457f3952cb20787f5
Muditha256 <muditharandeep123@gmail.com>
2024-11-12 17:27:28 +0530

    tailwind css applying and 1st routing part

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { AppoinmentManageComponent } from './appoinment-manage/appoinment-manage.component';
 import { PatientManageComponent } from './page/patient-manage/patient-manage.component';
+import { LoginPageComponent } from './page/login-page/login-page.component';
+import { DashBordPageComponent } from './page/dash-bord-page/dash-bord-page.component';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [
+    {
+        path:"",
+        component:LoginPageComponent
+    },
     {
         path:"appoinment",
         component:AppoinmentManageComponent
@@ -10,5 +17,20 @@ export const routes: Routes = [
     {
         path:"patient",
         component:PatientManageComponent
+    },
+    {
+        path:"dashbord",
+        component:DashBordPageComponent,
+        children:[
+            {
+                path:"appoinment",
+                component:AppoinmentManageComponent
+            },
+            {
+                path:"patient",
+                component:PatientManageComponent
+            }
+            
+        ]
     }
 ];
